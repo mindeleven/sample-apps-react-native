751d7c1caa880146d9363e4dca2e14e52e3215db
Jürgen Kober <mindeleven@posteo.de>
2020-10-19 15:21:22 +0200

    Added new components file for CounterScreen, hooked up the file in HomeScreen and App.

---
+++ rn-starter/src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View, Button } from "react-native";
+
+const CounterScreen = () => {
+  return(
+    <View>
+      <Text>Counter Screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
