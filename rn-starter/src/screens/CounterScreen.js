7eb82e4861a835b2f9d428b882cf9b1ff205bb27
Jürgen Kober <mindeleven@posteo.de>
2020-10-20 15:03:37 +0200

    Inplemeted a first state example in CounterScreen.js

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [ counter, setCounter ] = useState(0);
   return(
     <View>
-      <Button title="Increase" />
-      <Button title="Decrease" />
-      <Text>Counter Screen</Text>
+      <Button
+        title="Increase"
+        onPress={() => {
+          setCounter(counter + 1);
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          setCounter(counter - 1);
+        }}
+      />
+      <Text>Current Count: {counter}</Text>
     </View>
   )
 };
