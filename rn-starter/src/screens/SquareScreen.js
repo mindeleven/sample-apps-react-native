c9989eb4aeaa20a550c85567ab5b310fb15ecfa5
Jürgen Kober <mindeleven@posteo.de>
2020-10-21 14:41:04 +0200

    Added new component file SquareScreen.js with some boilerplate code.

---
+++ rn-starter/src/screens/SquareScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react-native";
+
+const SquareScreen = () => {
+  return(
+    <View>
+      <Text>Square Screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default SquareScreen;
