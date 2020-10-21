be7d266e90fa9a630c3f844faa3f6029f7d3505f
Jürgen Kober <mindeleven@posteo.de>
2020-10-21 15:01:28 +0200

    Imported the ColorCounter component into SquareScreen.

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Text, StyleSheet, View } from "react-native";
+import ColorCounter from '../components/ColorCounter';
 
 const SquareScreen = () => {
   return(
     <View>
-      <Text>Square Screen</Text>
+      <ColorCounter />
+      <ColorCounter />
+      <ColorCounter />
     </View>
   )
 };
