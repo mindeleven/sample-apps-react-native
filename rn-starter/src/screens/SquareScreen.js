5297088ef1154f9e55298ea3811792291cd5aea3
Jürgen Kober <mindeleven@posteo.de>
2020-10-25 11:06:38 +0100

    Fixed typo.

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -9,15 +9,15 @@ const reducer = (state, action) =>  {
   // action === {type: 'change_red' || 'change_green' || 'change_blue', payload: 15 || -15  }
   switch (action.type) {
     case 'change_red':
-      return state.red + action.payload > 255 || state.red + action.payload < 0)
+      return state.red + action.payload > 255 || state.red + action.payload < 0
         ? state
         : { ...state, red: state.red + action.payload };
     case 'change_green':
-      return state.green + action.payload > 255 || state.green + action.payload < 0)
+      return state.green + action.payload > 255 || state.green + action.payload < 0
         ? state
         : { ...state, green: state.green + action.payload };
     case 'change_blue':
-      return state.blue + action.payload > 255 || state.blue + action.payload < 0)
+      return state.blue + action.payload > 255 || state.blue + action.payload < 0
         ? state
         : { ...state, blue: state.blue + action.payload };
     default:
