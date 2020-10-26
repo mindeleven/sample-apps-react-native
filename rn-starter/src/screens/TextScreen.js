c46933857ff8b9c0a0aa5a750a82967597dc9763
Jürgen Kober <mindeleven@posteo.de>
2020-10-26 14:35:43 +0100

    Minor change to add form validation example.

--- rn-starter/src/screens/TextScreen.js
+++ rn-starter/src/screens/TextScreen.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, StyleSheet, View, TextInput } from "react-native";
 
 const TextScreen = () => {
+  const [ password, setPassword ] = useState('');
+
   return(
     <View>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value="Smoo Foo"
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
+        {password.length <= 5
+          ? <Text>Passwort must be longer than 5 characters</Text>
+          : null
+        }
     </View>
   )
 };
