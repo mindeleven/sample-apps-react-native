df91e2aa1d7e0136a75d43a06934e56dfcd0b7c5
Jürgen Kober <mindeleven@posteo.de>
2020-10-26 17:37:37 +0100

    Added some styling to demonstrate flex box.

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -4,21 +4,31 @@ import { Text, StyleSheet, View } from "react-native";
 const BoxScreen = () => {
   return(
     <View style={styles.viewStyle}>
-      <Text style={styles.textStyle}>BoxScreen</Text>
+      <Text style={styles.textOneStyle}>Child #1</Text>
+      <Text style={styles.textTwoStyle}>Child #2</Text>
+      <Text style={styles.textThreeStyle}>Child #3</Text>
     </View>
   )
 };
 
 const styles = StyleSheet.create({
   viewStyle:{
-    borderWidth: 1,
-    borderColor: 'black'
+    borderWidth: 3,
+    borderColor: 'black',
+    height: 200
   },
-  textStyle:{
-    borderWidth: 1,
+  textOneStyle:{
+    borderWidth: 3,
+    borderColor: 'red'
+  },
+  textTwoStyle:{
+    borderWidth: 3,
     borderColor: 'red',
-    marginVertical: 20,
-    marginHorizontal: 20
+    alignSelf: 'flex-end'
+  },
+  textThreeStyle:{
+    borderWidth: 3,
+    borderColor: 'red'
   }
 });
 
