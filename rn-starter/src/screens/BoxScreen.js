f071e0931e0be023ea4866297af4f87aece1e3ce
Jürgen Kober <mindeleven@posteo.de>
2020-10-28 13:38:22 +0100

    Added some styling to demonstrate positioning.

--- rn-starter/src/screens/BoxScreen.js
+++ rn-starter/src/screens/BoxScreen.js
@@ -15,18 +15,18 @@ const styles = StyleSheet.create({
   viewStyle:{
     borderWidth: 3,
     borderColor: 'black',
-    height: 200,
-    alignItems: 'center'
+    height: 200
   },
   textOneStyle:{
     borderWidth: 3,
+    fontSize: 60,
     borderColor: 'red'
   },
   textTwoStyle:{
     borderWidth: 3,
     borderColor: 'red',
-    position: 'absolute',
-    fontSize: 18
+    fontSize: 18,
+    top: 10
   },
   textThreeStyle:{
     borderWidth: 3,
