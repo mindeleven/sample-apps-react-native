b6018ba262221d5d981a148ca6ed6c83f4b12a2e
Jürgen Kober <mindeleven@posteo.de>
2020-10-30 14:40:12 +0100

    Added some scaffolding and styling for the SearchBar component

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   return (
     <View>
+      <SearchBar />
       <Text>Search Screen component</Text>
     </View>
   );
