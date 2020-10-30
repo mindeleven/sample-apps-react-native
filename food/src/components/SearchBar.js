73766d0ebed41d28705556fdea0af604ffc2fd0b
Jürgen Kober <mindeleven@posteo.de>
2020-10-30 15:15:58 +0100

    Added search icon from @expo/vector-icons

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
     <View style={styles.background} >
+      <Feather name="search" size={30} />
       <Text>Search Bar component</Text>
     </View>
   );
@@ -11,7 +13,7 @@ const SearchBar = () => {
 
 const styles = StyleSheet.create({
   background: {
-    backgroundColor: '#ebe1e1',
+    backgroundColor: '#EBE1E1',
     height: 50,
     borderRadius: 5,
     marginHorizontal: 15
