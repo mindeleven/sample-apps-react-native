76b576560cf59aa3d3be27a5b2b57350f542d45f
Jürgen Kober <mindeleven@posteo.de>
2020-10-30 15:40:09 +0100

    Added TextInput with some styling"

--- food/src/components/SearchBar.js
+++ food/src/components/SearchBar.js
@@ -1,22 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 const SearchBar = () => {
   return (
-    <View style={styles.background} >
+    <View style={styles.backgroundStyle} >
       <Feather name="search" size={30} />
-      <Text>Search Bar component</Text>
+      <TextInput
+        style={styles.inputStyle}
+        placeholder="Search"
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  background: {
+  backgroundStyle: {
     backgroundColor: '#EBE1E1',
     height: 50,
     borderRadius: 5,
-    marginHorizontal: 15
+    marginHorizontal: 15,
+    flexDirection: 'row'
+  },
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 1
   }
 });
 
