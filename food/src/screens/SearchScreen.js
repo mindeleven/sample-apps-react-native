9555ca9274056f5c7e11b53959eac4000332e7a6
Jürgen Kober <mindeleven@posteo.de>
2020-11-06 15:12:41 +0100

    Added ScrollView element to SearchScreen

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
@@ -24,18 +24,20 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
-      <ResultsList
-        results={filterResultsByPrice('$')}
-        title="Cost Effective"
-      />
-      <ResultsList
-        results={filterResultsByPrice('$$')}
-        title="Bit Pricier"
-      />
-      <ResultsList
-        results={filterResultsByPrice('$$$')}
-        title="Big Spender"
-      />
+      <ScrollView>
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective"
+        />
+        <ResultsList
+          results={filterResultsByPrice('$$')}
+          title="Bit Pricier"
+        />
+        <ResultsList
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender"
+        />
+      </ScrollView>
     </View>
   );
 };
