842ea57bb1a1814ee0d824ac3a3bef394deeccce
Jürgen Kober <mindeleven@posteo.de>
2020-11-08 16:11:27 +0100

    Integrated ResultsShowScreen

---
+++ food/src/screens/ResultsShowScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ResultsShowScreen = () => {
+  return (
+    <View>
+      <Text>Results show screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsShowScreen;
