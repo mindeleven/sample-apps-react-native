162070176ff19aa620188a1b453f937c12934d51
Jürgen Kober <mindeleven@posteo.de>
2020-11-08 15:57:36 +0100

    Added some styling to the search bar

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -23,7 +23,7 @@ const SearchScreen = () => {
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
+
       <ScrollView>
         <ResultsList
           results={filterResultsByPrice('$')}
