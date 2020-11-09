f9de0fbe75fc91617603438871b964d859a49f5e
Jürgen Kober <mindeleven@posteo.de>
2020-11-09 12:20:15 +0100

    Added FlatList element to display a list of images

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image } from 'react-native';
 import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({ navigation }) => {
@@ -17,13 +17,32 @@ const ResultsShowScreen = ({ navigation }) => {
     getResult(id)
   }, []);
 
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>Results show screen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({item}) => {
+          return (<Image
+            source={{ uri: item }}
+            style={styles.image} 
+          />)
+        }}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300
+  }
+});
 
 export default ResultsShowScreen;
