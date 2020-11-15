b1d52bd51ede8382b60142fc32ab37493d1570ac
Jürgen Kober <mindeleven@posteo.de>
2020-11-15 11:56:12 +0100

    Adapting the useContext call to the changed data structure in the Blog Provider

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -3,13 +3,13 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const blogPosts = useContext(BlogContext);
+  const { data, addBlogPosts } = useContext(BlogContext);
 
   return (
     <View>
       <Text>Index Screen</Text>
       <FlatList
-        data={blogPosts}
+        data={data}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({item}) => {
           return <Text>{item.title}</Text>;
