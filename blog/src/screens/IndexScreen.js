94bdbf7cceffb8b6fcb5c535032ed6f90f45cf59
Jürgen Kober <mindeleven@posteo.de>
2020-11-16 17:10:32 +0100

    Added and icon and some styling to the item list

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,13 +1,13 @@
 import React, { useContext } from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text>Index Screen</Text>
       <Button
         title="Add Post"
         onPress={() => addBlogPost()}
@@ -16,13 +16,34 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({item}) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Feather name="trash" style={styles.icon} />
+            </View>
+          );
         }}
       />
     </View>
   )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 18
+  }
+
+});
 
 export default IndexScreen;
