96d96d888fed899d86021a8c3c2eff6e61dd4b9b
Jürgen Kober <mindeleven@posteo.de>
2020-11-16 12:11:00 +0100

    Refactoring BlogContext to make use of createDataContext

--- blog/src/context/createDataContext.js
+++ blog/src/context/createDataContext.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 
 export default (reducer, actions, initialState) => {
-  const Context = react.createContext();
+  const Context = React.createContext();
 
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
