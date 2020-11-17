b4fbb3877301bb8c1f341ec22131c845a83dd6d2
Jürgen Kober <mindeleven@posteo.de>
2020-11-17 17:12:41 +0100

    Added new screen CreateScreen

---
+++ blog/src/screens/CreateScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const CreateScreeen = () => {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  );
+}
+
+export default CreateScreeen;
