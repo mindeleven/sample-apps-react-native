5a5c1a53674c50dd4c6ff4a5e052084663a80c55
Jürgen Kober <mindeleven@posteo.de>
2020-11-21 12:53:10 +0100

    Created basic form component to outsource TextInput elements

---
+++ blog/src/components/BlogPostForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+
+
+const BlogPostForm = () => {
+  return <Text>BlogPostForm</Text>
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogPostForm;
