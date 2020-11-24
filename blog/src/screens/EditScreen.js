3f3e9aa02a5a08013ee8532f508f2b96f18706d6
Jürgen Kober <mindeleven@posteo.de>
2020-11-24 12:54:57 +0100

    Passing inital values to BlogPostForm

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -13,8 +13,13 @@ const EditScreen = ({ navigation }) => {
   // const [title, setTitle] = useState(blogPost.title);
   // const [content, setContent] = useState(blogPost.content);
 
-  return <BlogPostForm />;
-
+  return (
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content}} 
+      onSubmit={(title, content) => {
+        console.log(title, content)
+      }}
+    />);
 }
 
 const styles = StyleSheet.create({
