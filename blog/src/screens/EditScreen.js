1508604c8886a9a635b7ce6bf46a7e55bde11b46
Jürgen Kober <mindeleven@posteo.de>
2020-11-24 15:40:36 +0100

    Made us of action function addBlogPost in EditScreen

--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -4,20 +4,20 @@ import { Context } from '../context/BlogContext';
 import BlogPostForm from '../components/BlogPostForm';
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
-  const { addBlogPost } = useContext(Context);
+  const id = navigation.getParam('id');
+  const { state, editBlogPost } = useContext(Context);
 
   const blogPost = state.find(
-    (blogPost) => blogPost.id === navigation.getParam('id')
+    (blogPost) => blogPost.id === id
   );
   // const [title, setTitle] = useState(blogPost.title);
   // const [content, setContent] = useState(blogPost.content);
 
   return (
     <BlogPostForm
-      initialValues={{ title: blogPost.title, content: blogPost.content}} 
+      initialValues={{ title: blogPost.title, content: blogPost.content}}
       onSubmit={(title, content) => {
-        console.log(title, content)
+        editBlogPost(id, title, content);
       }}
     />);
 }
