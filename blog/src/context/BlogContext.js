91bdf88202115a059afa101335b1efd87eb5ecef
Jürgen Kober <mindeleven@posteo.de>
2020-11-26 11:01:10 +0100

    Added missing return statement to edit_blogpost action

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -6,7 +6,7 @@ const blogReducer = (state, action) => {
     case 'edit_blogpost':
       //return state;
       return state.map((blogPost) => {
-        blogPost.id === action.payload.id ? action.payload : blogPost;
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
       })
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload);
