9d3e6cad154d7c4637f1bfb6503a31063b4542b2
Jürgen Kober <mindeleven@posteo.de>
2020-12-03 14:53:16 +0100

    Removing the initial state from the app

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -66,5 +66,5 @@ const deleteBlogPost = (dispatch) => {
 export const { Context, Provider } = createDataContext(
   blogReducer,
   { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts },
-  [{ title: 'Test Post No. 1', content: 'Content for Test Post No. 1', id: 666 }]
+  []
 );
