4770e88a967c20c9f2a995a80f2ce5acdf3e96b7
Jürgen Kober <mindeleven@posteo.de>
2020-12-03 15:15:23 +0100

    Making use of addBlogPosts in the IndexSceen with the useEffect hook

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import {
   StyleSheet, Text, View, FlatList, Button, TouchableOpacity
 } from 'react-native';
@@ -6,7 +6,11 @@ import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = ({ navigation }) => {
-  const { state, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+
+  useEffect(() => {
+    getBlogPosts();
+  }, []);
 
   return (
     <View>
