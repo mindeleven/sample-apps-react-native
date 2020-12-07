4a40f8965db4704334f81513ba0c42afc4bdc067
Jürgen Kober <mindeleven@posteo.de>
2020-12-07 16:51:10 +0100

    Added listener to useEffect() in IndexScreen

--- blog/src/screens/IndexScreen.js
+++ blog/src/screens/IndexScreen.js
@@ -10,6 +10,14 @@ const IndexScreen = ({ navigation }) => {
 
   useEffect(() => {
     getBlogPosts();
+
+    const listener = navigation.addListener('didFocus', () => {
+      getBlogPosts();
+    });
+
+    return () => {
+      listener.remove();
+    };
   }, []);
 
   return (
