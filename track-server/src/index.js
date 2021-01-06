3db211ce52b933730663924004cd2b887032e138
Jürgen Kober <mindeleven@posteo.de>
2021-01-06 10:46:51 +0100

    Adding auth routes to the express app

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0.lmkv7.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
