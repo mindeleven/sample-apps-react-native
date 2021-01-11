a4fa54963f54588b36460f673c6a603c2450a210
Jürgen Kober <mindeleven@posteo.de>
2021-01-11 17:20:57 +0100

    Adding bcrypt hashing algorithm to the user model"

--- track-server/src/models/user.js
+++ track-server/src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -10,6 +11,27 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+userSchema.pre('save', function() {
+  const user = this;
+  if(!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if(err) {
+      next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if(err) {
+        next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
 });
 
 mongoose.model('User', userSchema);
