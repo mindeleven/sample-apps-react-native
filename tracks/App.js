1429660e27b14e59e64cb30e253d06f0248fcc0e
Jürgen Kober <mindeleven@posteo.de>
2021-02-08 15:23:37 +0100

    Remove superfluous comments

--- tracks/App.js
+++ tracks/App.js
@@ -4,13 +4,12 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { AccountScreen } from './src/screens/AccountScreen';
-/***
 import { SigninScreen } from './src/screens/SigninScreen';
 import { SignupScreen } from './src/screens/SignupScreen';
 import { TrackCreateScreen } from './src/screens/TrackCreateScreen';
 import { TrackDetailScreen } from './src/screens/TrackDetailScreen';
 import { TrackListScreen } from './src/screens/TrackListScreen';
-***/
+
 
 export default function App() {
   return (
