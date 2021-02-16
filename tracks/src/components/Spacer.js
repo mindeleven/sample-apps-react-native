6228069e7ccc3398d26009918222d21dc9ba5937
Jürgen Kober <mindeleven@posteo.de>
2021-02-16 11:59:04 +0100

    Elaborating on the  helper component to apply spacing to form elements

--- tracks/src/components/Spacer.js
+++ tracks/src/components/Spacer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-const Spacer = () => {
-
+const Spacer = ({ children }) => {
+  return <View style={styles.spacer}>{children}</View>
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  spacer: {
+    margin: 15
+  }
+});
 
 export default Spacer;
