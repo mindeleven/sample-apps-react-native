a456fe182908247e0ccaa7ebb6ab7d81f0b55bd2
Jürgen Kober <mindeleven@posteo.de>
2021-02-16 11:41:28 +0100

    Integrating react-native-elements and replacing form components wit components

--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
 
 const SignupScreen = ({ navigation }) => {
   return <>
-    <Text style={{ fontSize: 48 }}>Signup Screen</Text>
+    <Text h3>Sign Up for Tracker</Text>
+    <Input label="Email" />
+    <Input label="Password" />
     <Button
-      title="Go to Signin"
-      onPress={() => navigation.navigate("Signin")}
-    />
-    <Button
-      title="Go to main flow"
-      onPress={() => navigation.navigate("mainFlow")}
+      title="Sign Up"
     />
   </>
 };
