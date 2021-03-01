eaaa9ccf17e0986d07426385a081fbd7e119a5fa
Jürgen Kober <mindeleven@posteo.de>
2021-03-01 15:33:09 +0100

    Scaffolding for three different action functions

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -7,6 +7,36 @@ const authReducer = (state, action) => {
   }
 };
 
+const signup = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to signup with email and password
+
+    // if we sign up, modify our state
+    // and say that we're authenticated
+
+    // if sign in fails, reflect error message
+
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    // make api request to signin with email and password
+
+    // if we sign in, modify our state
+    // and say that we're authenticated
+
+    // if sign in fails, reflect error message
+
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+
+  };
+};
+
 export const { Provider, Context } = createDataContext(
   authReducer,
   {},
