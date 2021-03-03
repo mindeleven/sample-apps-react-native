2e6b232ee05e237976894bddb20299071d39619f
Jürgen Kober <mindeleven@posteo.de>
2021-03-03 13:04:22 +0100

    Adding function that allows non-React components to trigger navigation

--- tracks/src/navigationRef.js
+++ tracks/src/navigationRef.js
@@ -1,5 +1,16 @@
+import { navigationActions } from 'react-navigation';
+
 let navigator;
 
 export const setNavigator = (nav) => {
   navigator = nav;
 };
+
+export const navigate = (routeName, params) => {
+  navigator.dispatch(
+    navigationActions.navigate({
+      routeName,
+      params
+    })
+  );
+}
