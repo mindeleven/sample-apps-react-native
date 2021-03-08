e387f175608fcd1337311e2ddf16cf00d1f15af0
Jürgen Kober <mindeleven@posteo.de>
2021-03-08 17:44:16 +0100

    Elaboration of the signin form

--- tracks/src/screens/SigninScreen.js
+++ tracks/src/screens/SigninScreen.js
@@ -1,10 +1,38 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
-const SigninScreen = () => {
-  return <Text style={{ fontSize: 48 }}>Signin Screen</Text>;
+const SigninScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText="Sign In to your Account"
+        errorMessage=""
+        onSubmit={() => {}}
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        navigation={navigation}
+        text="Don't have an account? Sign up instead"
+        routeName="Signup"
+      />
+    </View>
+  )
 };
 
-const styles = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 100
+  }
+});
 
 export default SigninScreen;
