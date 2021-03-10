2df09f54ca79aa412b5a11ec990ead6b3d61e635
Jürgen Kober <mindeleven@posteo.de>
2021-03-10 14:38:50 +0100

    Exporting clearErrorMessage function

--- tracks/src/context/AuthContext.js
+++ tracks/src/context/AuthContext.js
@@ -69,6 +69,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  { signin, signout, signup },
+  { signin, signout, signup, clearErrorMessage },
   { token: null, errorMessage: '' }
 );
