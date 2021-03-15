ec4f8b87de655609903587994a5623ffba785053
Jürgen Kober <mindeleven@posteo.de>
2021-03-15 15:29:01 +0100

    Changed import source of SafeAreaView to react-native-safe-area-context instead of react-navigation to get rid of deprecated warning

--- tracks/src/screens/AccountScreen.js
+++ tracks/src/screens/AccountScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Button } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Context as AuthContext } from '../context/AuthContext';
 import Spacer from '../components/Spacer';
 
