fd6eb1426a8fef1d12ecabedaa2f7a8c90889061
Jürgen Kober <mindeleven@posteo.de>
2021-03-15 15:47:21 +0100

    Scaffolding for Map component

---
+++ tracks/src/components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const Map = () => {
+  return <Text>I'm a Map</Text>;
+}
+
+const styles = StyleSheet.create({
+});
+
+export default Map;
